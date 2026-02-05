d53d78d6f1f3bf8c40af7834463166562e7e6302
BludosDev <bludosofficial+67@gmail.com>
2026-02-05 14:46:44 +0530

    Initial commit

---
+++ src/main/java/com/bludos/bxtreme/Main.java
@@ -0,0 +1,10 @@
+package com.bludos.bxtreme;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod("bxtreme")
+public class Main {
+    public Main() {
+        System.out.println("Hello from bxtreme!");
+    }
+}
