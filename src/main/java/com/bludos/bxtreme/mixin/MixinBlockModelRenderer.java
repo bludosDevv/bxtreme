a0b5d0c00adfc8aec7bd238dacaaf45e07593508
BludosDevv <bludosofficial@gmail.com>
2026-02-05 21:15:10 +0530

    update MixinBlockModelRenderer.java

--- src/main/java/com/bludos/bxtreme/mixin/MixinBlockModelRenderer.java
+++ src/main/java/com/bludos/bxtreme/mixin/MixinBlockModelRenderer.java
@@ -1,22 +1,20 @@
 package com.bludos.bxtreme.mixin;
 
 import com.bludos.bxtreme.Main;
-import com.bludos.bxtreme.render.GreedyMeshBuilder;
 import com.mojang.blaze3d.vertex.PoseStack;
 import com.mojang.blaze3d.vertex.VertexConsumer;
-import net.minecraft.client.renderer.block.BlockModelRenderer;
+import net.minecraft.client.renderer.block.ModelBlockRenderer;
 import net.minecraft.client.resources.model.BakedModel;
 import net.minecraft.core.BlockPos;
-import net.minecraft.core.Direction;
+import net.minecraft.util.RandomSource;
 import net.minecraft.world.level.BlockAndTintGetter;
 import net.minecraft.world.level.block.state.BlockState;
-import net.minecraft.util.RandomSource;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(BlockModelRenderer.class)
+@Mixin(ModelBlockRenderer.class)
 public class MixinBlockModelRenderer {
     
     /**
@@ -33,6 +31,6 @@ public class MixinBlockModelRenderer {
         }
         
         // Greedy culling is applied - this is just a marker
-        // The actual culling happens in shouldRenderFace check0
+        // The actual culling happens in shouldRenderFace checks
     }
 }
